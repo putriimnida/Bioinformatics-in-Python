ae94355c0c10a39e82cc34cf592d7c5852ec67ee
putriimnida <156336254+putriimnida@users.noreply.github.com>
2024-02-19 11:30:45 +0700

    Add files via upload

---
+++ Count-nucleotides.py
@@ -0,0 +1,19 @@
+# DNA Toolset/Code testing file
+from DNAToolKit import *
+import random
+
+# Creating a random DNA sequence for testing:
+randDNAStr = ''.join([random.choice(Nucleotides)for nuc in range(20)])
+
+print(validateSeq(rndDNAStr))
+
+# -- Output -- #
+# ATTGCTCATTAGGTACGATCAAAAATTCGCTTAAGCCTTAAGGCAATTTAAACGCGGCTAA
+# ------------ #
+
+# Counting a random DNA sequence for testing:
+print(countNucFrequency(randDNAStr))
+
+# -- Output -- #
+# {'A': 3, 'C': 7, 'G': 4, 'T': 6}
+# ------------ #
