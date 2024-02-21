898cd8f3a9fa64bdb5caa853df28a6ec5b8bd47b
putriimnida <156336254+putriimnida@users.noreply.github.com>
2024-02-21 08:37:36 +0700

    Add files via upload

---
+++ DNAToolKit.py
@@ -0,0 +1,19 @@
+# DNA Toolkit file
+
+import collections
+Nucleotides = ("A", "C", "G", "T")
+
+# Check the sequence to make sure it is a DNA string
+def validateSeq(dna_seq):
+    tmpseq = dna_seq.upper()
+    for nuc in tmpseq:
+        if nuc not in Nucleotides:
+            return False
+    return tmpseq
+
+def countNucFrequency(seq):
+    tmpFreqDict = {"A": 0, "C": 0, "G": 0, "T": 0}
+    for nuc in seq:
+        tmpFreqDict[nuc] += 1
+    return tmpFreqDict
+#return dict(collections.Counter(seq))
