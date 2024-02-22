7c2c717c117c377f0e1694eadc09cbbc34dbe8e6
putriimnida <156336254+putriimnida@users.noreply.github.com>
2024-02-22 10:10:44 +0700

    Add files via upload
    DNA transcription toolkit, run, and output

---
+++ DNA transcription.py
@@ -0,0 +1,14 @@
+# DNA Toolkit
+# def transcription(seq):
+    # DNA -> RNA transcription
+    # return seq.replace("T", "U")
+
+# DNA transcription
+from DNAToolKit import *
+DNAStr = "ATTGCTCATTAGGTACGATCAAAAATTCGCTTAAGCCTTAAGGCAATTTAAACGCGGCTAA"
+print(transcription(DNAStr))
+
+# -- Output -- #
+# AUUGCUCAUUAGGUACGAUCAAAAAUUCGCUUAAGCCUUAAGGCAAUUUAAACGCGGCUAA
+# ------------ #
+
