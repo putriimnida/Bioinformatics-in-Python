26f02b04a2d02cb0b145a7bf1b76390fbd72ca25
putriimnida <156336254+putriimnida@users.noreply.github.com>
2024-03-04 11:32:15 +0700

    Add files via upload

---
+++ Structures.py
@@ -0,0 +1,5 @@
+collections
+
+Nucleotides = ("A", "C", "G", "T")
+DNAReverseComplement = {"A": "T", "T": "A", "G": "C", "C": "G"}
+DNAStr = "ATTGCTCATTAGGTACGATCAAAAATTCGCTTAAGCCTTAAGGCAATTTAAACGCGGCTAA"
