fc3abaae4b93a59fd0725d4ec7f81984473524a6
putriimnida <156336254+putriimnida@users.noreply.github.com>
2024-03-18 10:48:42 +0700

    Update DNAToolKit.py
    add coloring nucleotides toolkit

--- DNAToolKit.py
+++ DNAToolKit.py
@@ -2,6 +2,10 @@
 
 import collections
 Nucleotides = ("A", "C", "G", "T")
+DNAReverseComplement = {"A": "T", "T": "A", "G": "C", "C": "G"}
+DNAStr = "ATTGCTCATTAGGTACGATCAAAAATTCGCTTAAGCCTTAAGGCAATTTAAACGCGGCTAA"
+
+
 
 # Check the sequence to make sure it is a DNA string
 def validateSeq(dna_seq):
@@ -17,3 +21,40 @@ def countNucFrequency(seq):
         tmpFreqDict[nuc] += 1
     return tmpFreqDict
 #return dict(collections.Counter(seq))
+
+def transcription(seq):
+    # DNA -> RNA transcription
+    return seq.replace("T", "U")
+
+def reverse_complement(seq):
+    # swapping adenine with thymine and guanine with cytosine.
+    # reversing newly generated string
+    # Pythonic approach. A little faster solution.
+    # mapping = str.maketrans('ATCG', 'TAGC')
+    # return seq.translate(mapping)[::-1]
+    return ''.join([DNAReverseComplement[nuc] for nuc in seq])[::-1]
+
+
+
+
+#Coloring nucleotides
+def colored(seq):
+    bcolors = {
+        'A': '\033[92m',
+        'C': '\033[94m',
+        'G': '\033[93m',
+        'T': '\033[91m',
+        'U': '\033[91m',
+        "reset": '\033[0;0m'
+
+    }
+
+    tmpStr = ""
+
+    for nuc in seq:
+        if nuc in bcolors:
+            tmpStr += bcolors[nuc] + nuc
+        else:
+            tmpStr += bcolors['reset'] + nuc
+
+    return tmpStr + '\033[0;0m'
