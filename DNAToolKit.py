03d3649efae26d9d0f1860b30fb5b7762146dd7d
putriimnida <156336254+putriimnida@users.noreply.github.com>
2024-06-06 09:52:23 +0700

    Add files via upload

--- DNAToolKit.py
+++ DNAToolKit.py
@@ -59,7 +59,30 @@ def colored(seq):
 
 
 
+
 # GC Content Calculation
 def gc_content(seq):
     return round((seq.count('C') + seq.count('G')) /len(seq) * 100)
 
+
+# DNA Toolkit file
+import collections
+
+Nucleotides = ["A", "C", "G", "T"]
+
+
+# Check the sequence to make sure it is a DNA String
+def validateSeq(dna_seq):
+    tmpseq = dna_seq.upper()
+    for nuc in tmpseq:
+        if nuc not in Nucleotides:
+            return False
+    return tmpseq
+
+
+def countNucFrequency(seq):
+    tmpFreqDict = {"A": 0, "C": 0, "G": 0, "T": 0}
+    for nuc in seq:
+        tmpFreqDict[nuc] += 1
+    return tmpFreqDict
+    # return dict(collections.Counter(seq))
